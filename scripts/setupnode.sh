549e522cc8c9b36b301c2ed5c1b390096f17696e
Rushi Agrawal <rushi.agr@gmail.com>
2014-11-22 07:32:33 +0000

    add script to add fqdn
    darn, cant stop using shell, and start using puppet

--- scripts/setupnode.sh
+++ scripts/setupnode.sh
@@ -2,3 +2,7 @@
 # assumes cloudguru directory is placed directly under home dir
 cd ~/cloudguru
 librarian-puppet install
+
+# setup FQDN
+HOSTNAME=$(hostname)
+echo "127.0.1.1 $HOSTNAME.example.com $HOSTNAME" | sudo tee --append /etc/hosts
