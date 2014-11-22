0b634523370b5ea1a494f85df6fc0b509e7235e3
Rushi Agrawal <rushi.agr@gmail.com>
2014-11-22 08:52:23 +0000

    update rushi_setupnode to take glance image from local machine only

--- scripts/rushi_setupnode.sh
+++ scripts/rushi_setupnode.sh
@@ -9,3 +9,22 @@ cd ~/cloudguru
 
 mkdir modules
 rsync -r $HOST_MC_USER@$HOST_MC_IP:$HOST_MODULES_SOURCE modules
+
+scp -r r@$HOST_MC_IP:/home/r/src/cloudguru/cirros032.img .
+
+sudo puppet apply site.pp --debug --modulepath modules/
+
+sudo cp ssl/fake.crt /usr/local/share/ca-certificates/
+sudo update-ca-certificates
+
+source adminrc
+
+glance image-create --name cirros032 --disk-format qcow2 --container-format bare --is-public True --progress < cirros-0.3.2-x86_64-disk.img
+
+## Nova
+nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
+nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
+
+nova keypair-add rushi >> rushi.pem
+
+nova boot --image cirros032 --flavor m1.tiny inst_one
