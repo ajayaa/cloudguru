69a38aef9e3a3b4ad4f6d7cc4aa255678767604b
Rushi Agrawal <rushi.agr@gmail.com>
2014-11-22 09:58:46 +0000

    add rabbitmq include
    was removed by accident

--- rabbit.pp
+++ rabbit.pp
@@ -2,6 +2,7 @@
 
 # NOTE: need to run with this code for the first time. This code can and should
 # be cleaned up ASAP.
+include rabbitmq
 rabbitmq_user { 'rabbituser':
   admin     => true,
   password  => 'rabbitpass',
