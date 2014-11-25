11c3a2979a3426d9789808e2d27c9551567fac7d
Rushi Agrawal <rushi.agr@gmail.com>
2014-11-25 17:35:07 +0000

    add eth2 network configuration to pre-puppet-apply.sh

--- scripts/rushi/pre-puppet-apply.sh
+++ scripts/rushi/pre-puppet-apply.sh
@@ -6,6 +6,8 @@ set -eux
 # Sets FQDN
 # Adds SSL certificate
 
+# Assumes 2 nics - eth1 as mgmt interface, eth2 as data interface
+
 HOST_MC_IP=192.168.100.1
 HOST_MC_USER=r
 HOST_MODULES_SOURCE=/home/r/src/cloudguru/modules
@@ -20,3 +22,7 @@ scp -r r@$HOST_MC_IP:/home/r/src/cloudguru/cirros-0.3.2-x86_64-disk.img $CLOUDGU
 
 sudo cp $CLOUDGURU_DIR/ssl/fake.crt /usr/local/share/ca-certificates/
 sudo update-ca-certificates
+
+sudo rm /etc/network/interfaces
+sudo cp $CLOUDGURU_DIR/scripts/rushi/interfaces /etc/network/interfaces
+sudo ifdown eth2 && sudo ifup eth2
