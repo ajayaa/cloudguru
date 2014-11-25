fdcdaa63b4e3c4a5ed19492e72ad60d3872d8586
Rushi Agrawal <rushi.agr@gmail.com>
2014-11-25 19:56:26 +0000

    add script to validate cloud setup

---
+++ scripts/validate-cloud.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+set -eux
+
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=ChangeMe
+export OS_AUTH_URL=https://node1.example.com:5000/v2.0
+
+unset http_proxy
+
+keystone token-get
+keystone user-list
+keystone tenant-list
+
+glance image-list
+
+nova flavor-list
+nova list
+nova image-list
