43dc399f8ed7c8819eb27e57c34d827e8a2732a1
Rushi Agrawal <rushi.agr@gmail.com>
2014-12-07 20:09:04 +0530

    move certs dir ssl inside files

--- scripts/update-cert.sh
+++ scripts/update-cert.sh
@@ -3,6 +3,6 @@ set -eux
 
 # Updates certificates into the system
 
-sudo cp /home/vagrant/cloudguru/ssl/fake.crt /usr/local/share/ca-certificates/
+sudo cp /home/vagrant/cloudguru/files/ssl/fake.crt /usr/local/share/ca-certificates/
 sudo update-ca-certificates
 
