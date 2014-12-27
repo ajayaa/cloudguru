410bcfe5488611b6fbc93b274d1b8fb81f7d0aba
Rushi Agrawal <rushi.agr@gmail.com>
2014-12-27 16:55:11 +0530

    Update post-puppet-apply to include neutron setup

--- scripts/70-post-puppet-apply.sh
+++ scripts/70-post-puppet-apply.sh
@@ -11,7 +11,15 @@ nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
 
 nova flavor-create m1.nano 6 64 0 1
 
-neutron net-create mynet
-neutron subnet-create --name mynet-subnet mynet 10.0.0.0/24
+sudo ovs-vsctl add-br br-ex
+sudo ovs-vsctl add-port br-ex eth2
+
+neutron net-create private
+neutron subnet-create --name private-subnet private 10.0.0.0/24
+
+neutron net-create public --router:external True
+neutron subnet-create --name public-subnet public 192.168.200.0/24
+
 neutron router-create myrouter
-neutron router-interface-add myrouter mynet-subnet
+neutron router-interface-add myrouter private-subnet
+neutron router-gateway-set myrouter public
